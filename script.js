c9e7e5790d1ae8dc3d1dad2c5cb2a62d3612181a
Arif Imeri <arifimeri.11@gmail.com>
2025-03-19 00:36:59 +0100

    Added script.js and style.css

---
+++ script.js
@@ -0,0 +1,8 @@
+const searchBox = document.querySelector('.searchBox');
+const searchBtn = document.querySelector('.searchBtn');
+const recipeContainer = document.querySelector('.recipe-container');
+
+searchBtn.addEventListener('click',  (e) => {
+    e.preventDefault();
+    
+})
